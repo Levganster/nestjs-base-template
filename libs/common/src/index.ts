0c58d59bac9e5bfe248ea86430ba70b280321b75
idmaksim <dmaksim@tuta.io>
2024-12-08 14:58:16 +0300

    Add Prisma library configuration and remove unused Prisma utilities

--- libs/common/src/index.ts
+++ libs/common/src/index.ts
@@ -13,10 +13,6 @@ export * from './guards/permission.guard';
 export * from './interceptors/logger.interceptor';
 
 export * from './middlewares/logger.middleware';
-
-export * from './prisma/pagination.base';
-export * from './prisma/utils';
-
 export * from './services/prisma.service';
 
 export * from './types/jwt-payload';
