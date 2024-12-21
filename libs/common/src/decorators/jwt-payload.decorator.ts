cd448295b9941a7272c06e3892476e3e58a2934e
idmaksim <dmaksim@tuta.io>
2024-12-21 12:23:16 +0300

    Remove unused JWT payload decorator from the common library
    - Deleted the `jwt-payload.decorator.ts` file as it was no longer needed.
    - Updated the index file to reflect the removal of the JWT payload decorator export, streamlining the codebase.

--- libs/common/src/decorators/jwt-payload.decorator.ts
+++
@@ -1,7 +0,0 @@
-import { createParamDecorator, ExecutionContext } from '@nestjs/common';
-
-export const DecodeJwtPayload = createParamDecorator(
-  (data: unknown, context: ExecutionContext) => {
-    return context.switchToHttp().getRequest().user;
-  },
-);
