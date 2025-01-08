ed5494ab4b3967282167b81f293335ec05a8f692
idmaksim <dementevmaksim1657@gmail.com>
2025-01-08 16:24:13 +0300

    Add spec files to role repository, role service and token service

--- libs/role/src/role.module.ts
+++ libs/role/src/role.module.ts
@@ -1,13 +1,12 @@
 import { forwardRef, Module } from '@nestjs/common';
 import { RoleService } from './role.service';
-import { RoleController } from './role.controller';
 import { PermissionModule } from '@app/permissions';
 import { RoleRepository } from './role.repository';
 import { UsersModule } from '@app/users';
 import { PrismaModule } from '@app/prisma/prisma.module';
 
 @Module({
-  controllers: [RoleController],
+  controllers: [],
   providers: [RoleService, RoleRepository],
   exports: [RoleService],
   imports: [PermissionModule, UsersModule, PrismaModule],
