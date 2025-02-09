71e21ff4761be6602e63b13d7fcb5c214ba28479
idmaksim <dementevmaksim1657@gmail.com>
2025-02-09 13:31:28 +0300

    Add password removal interceptor to users controller
    - Imported RemovePasswordInterceptor from common interceptors
    - Applied UseInterceptors decorator to automatically remove password from user responses

--- apps/main/src/modules/users/users.controller.ts
+++ apps/main/src/modules/users/users.controller.ts
@@ -1,14 +1,16 @@
-import { Controller, Get, UseGuards } from '@nestjs/common';
+import { Controller, Get, UseGuards, UseInterceptors } from '@nestjs/common';
 import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
 import { JwtAuthGuard } from '@app/common/guards/auth.guard';
 import { DecodeUser } from '@app/common/decorators/decode-user.decorator';
 import { UserWithoutPassword } from '@app/common/types/user';
 import { UsersService } from '@app/users';
+import { RemovePasswordInterceptor } from '@app/common/interceptors/password.interceptor';
 
 @Controller('users')
 @ApiTags('Users')
 @UseGuards(JwtAuthGuard)
 @ApiBearerAuth()
+@UseInterceptors(RemovePasswordInterceptor)
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
